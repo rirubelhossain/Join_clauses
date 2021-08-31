f498e1a40b8ff5f78b0d681d56cc3c05c7af9cee
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-08-31 11:59:11 +0600

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB ;
-class PostController extends Controller
+class PostController extends Controller 
 {
     public function innerjoinclauses()
     {
-        $request = DB ::table('users')
+        $request = DB::table('users')
             ->join('posts','users.id','=','posts.user_id')
             ->select('users.name','posts.title','posts.body')
             ->get();
